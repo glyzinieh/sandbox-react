37ff1700cd0da1e01c73d7146451f6589e773030
Haruhiko Fuji <glyzinie.h@gmail.com>
2025-08-03 15:44:20 +0900

    最後のお掃除

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -50,19 +50,17 @@ function Board({ xIsNext, squares, onPlay}) {
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [xIsNext, setXIsNext] = useState(true);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history.slice(0, currentMove+1), nextSquares]);
+    setHistory([...history.slice(0, currentMove + 1), nextSquares]);
     setCurrentMove(history.length);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
